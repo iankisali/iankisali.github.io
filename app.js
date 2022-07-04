ffd4dedfc34a4725fad63d32e98115632df330ce
Ian Kisali <iankisali@gmail.com>
2022-07-04 16:20:24 +0300

    Updating my Portfolio

--- app.js
+++ app.js
@@ -32,3 +32,18 @@ function PageTransitions(params) {
     })
 }
 PageTransitions();
+
+/*(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
+    });
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
+*/

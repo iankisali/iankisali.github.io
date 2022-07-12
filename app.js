c50c5d93a1e794f8952f2838f66f74d860322c2f
Ian Kisali <iankisali@gmail.com>
2022-07-12 10:56:14 +0300

    added javascript file

--- app.js
+++ app.js
@@ -30,6 +30,13 @@ function PageTransitions(params) {
             element.classList.add('active');
         }
     })
+
+    //Toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click',()=>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 PageTransitions();
 
